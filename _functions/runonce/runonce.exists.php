ec5d673d35c9b969b91d904a73029d9ae92cebec
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-22 16:09:00 +0000

    Update runonce.exists.php

--- _functions/runonce/runonce.exists.php
+++ _functions/runonce/runonce.exists.php
@@ -1,7 +1,6 @@
 <?php
 
 function Puff_Runonce_Exists($Connection, $Runonce, $Active = true) {
-	$Runonce = htmlentities($Runonce, ENT_QUOTES, 'UTF-8');
 	$SQL = 'SELECT * FROM `Runonces` WHERE `Runonce`=\''.$Runonce.'\'';
 	if ( $Active ) {
 		$SQL .= ' AND `Active`=\'1\'';
